7256a8a9ce5769511393d9a47e395dd58c77bd19
Pat Deegan <pat.devel@psychogenic.com>
2024-04-27 13:42:57 -0400

    multi shuttle sdk test support

--- src/ttcontrol/factory/tt03p5.py
+++ src/ttcontrol/factory/tt03p5.py
@@ -3,14 +3,29 @@ import ttboard.util.shuttle_tests as st
 
 tt = DemoBoard.get()
 
-err = st.factory_test_bidirs(tt)
+
+# we have many SDK versions floating around
+# for tt03p5, some sad try blocks here to
+# account for changes
+try:
+    # testchip specific naming in latest sdk
+    err = st.factory_test_bidirs_03p5(tt)
+except AttributeError:
+    err = st.factory_test_bidirs(tt)
+
 okay = True
 
 if err is not None:
     print(f"error=factory_test_bidirs, {err}")
     okay = False
 
-err = st.factory_test_clocking(tt)
+
+try:
+    # testchip specific naming in latest sdk
+    err = st.factory_test_clocking_03p5(tt)
+except AttributeError:
+    err = st.factory_test_clocking(tt)
+
 if err is not None:
     print(f"error=factory_test_clocking, {err}")
     okay = False
