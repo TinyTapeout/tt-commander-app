5b9eaefc1fdda300933b15a3d8e9f884f90bbd7f
Pat Deegan <pat.devel@psychogenic.com>
2024-04-28 13:50:14 -0400

    feat(factory): multi shuttle sdk test support (#7)

--- src/ttcontrol/factory/default.py
+++ src/ttcontrol/factory/default.py
@@ -2,18 +2,19 @@ from ttboard.demoboard import DemoBoard
 import ttboard.util.shuttle_tests as st
 
 tt = DemoBoard.get()
-
-err = st.factory_test_bidirs(tt)
+try:
+    err = st.factory_test_clocking_04(tt)
+except AttributeError:
+    err = 'Unsupported Test Method'
+    
 okay = True
 
 if err is not None:
-    print(f"error=factory_test_bidirs, {err}")
-    okay = False
-
-err = st.factory_test_clocking(tt)
-if err is not None:
     print(f"error=factory_test_clocking, {err}")
     okay = False
+    
+    
+# TODO: bidir test through factory test project
 
 if okay:
     print("factory_test=OK")
