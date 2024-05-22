696225738fd64a7fcb259a04233823b314764a46
Uri Shaked <uri@urishaked.com>
2024-05-22 09:39:59 +0300

    fix(pinout): tt06+ project pinouts fail to load
    Instead of trying to retrieve the info.yaml and parse the pinout info on our own, simply get the pinout info from the tinytapeout shuttle index (https://github.com/TinyTapeout/tinytapeout-index)

--- src/components/PinoutPanel.tsx
+++ src/components/PinoutPanel.tsx
@@ -8,25 +8,42 @@ import {
   Typography,
 } from '@suid/material';
 import { For, Show, createResource } from 'solid-js';
-import YAML from 'yaml';
 import { deviceState } from '~/model/DeviceState';
-import { shuttle } from '~/model/shuttle';
-import { extractRepoFromURL } from '~/utils/github';
+import { Project, shuttle } from '~/model/shuttle';
+
+interface ExtraProjectInfo {
+  macro: string;
+  author: string;
+  description: string;
+  pinout: Record<string, string>;
+}
+
+const extraProjectInfo = new WeakMap<Project, ExtraProjectInfo>();
 
 export function PinoutPanel() {
   const selectedProject = () =>
     shuttle.projects.find((p) => p.address === deviceState.selectedDesign);
 
-  const [infoYaml] = createResource(async () => {
+  const [projectInfo] = createResource(async () => {
     const project = selectedProject();
     if (!project) {
       return null;
     }
-    const repoName = extractRepoFromURL(project.repo);
-    const response = await fetch(`https://raw.githubusercontent.com/${repoName}/main/info.yaml`);
-    const yaml = await response.text();
-    const projectInfo = YAML.parse(yaml);
-    return projectInfo;
+
+    const cached = extraProjectInfo.get(project);
+    if (cached) {
+      return cached;
+    }
+
+    const response = await fetch(
+      `https://index.tinytapeout.com/${shuttle.id}.json?fields=author,description,pinout&filter=${project.macro}`,
+    );
+    const json: { projects: ExtraProjectInfo[] } = await response.json();
+    const result = json.projects.find((p) => p.macro === project.macro);
+    if (result) {
+      extraProjectInfo.set(project, result);
+    }
+    return result;
   });
 
   const pins = [0, 1, 2, 3, 4, 5, 6, 7];
@@ -34,13 +51,11 @@ export function PinoutPanel() {
   return (
     <Stack mt={1}>
       <Typography variant="h6">
-        {infoYaml.loading ? 'Loading...' : infoYaml().documentation?.title ?? 'Error'}
-        <Show when={infoYaml()?.documentation}> by {infoYaml()?.documentation.author}</Show>
+        {projectInfo.loading ? 'Loading...' : selectedProject()?.title ?? 'Error'}
+        <Show when={projectInfo()}> by {projectInfo()?.author}</Show>
       </Typography>
       <Typography variant="body2" color="textSecondary">
-        {infoYaml.error
-          ? 'Error loading documentation'
-          : infoYaml()?.documentation?.description ?? ''}
+        {projectInfo.error ? 'Error loading documentation' : projectInfo()?.description ?? ''}
       </Typography>
       <Table>
         <TableHead>
@@ -51,15 +66,15 @@ export function PinoutPanel() {
             <TableCell>Bidirectional</TableCell>
           </TableRow>
         </TableHead>
-        <Show when={!infoYaml.loading && !infoYaml.error}>
+        <Show when={!projectInfo.loading && !projectInfo.error}>
           <TableBody>
             <For each={pins}>
               {(pinIndex) => (
                 <TableRow>
                   <TableCell>{pinIndex}</TableCell>
-                  <TableCell>{infoYaml().documentation.inputs?.[pinIndex] ?? ''}</TableCell>
-                  <TableCell>{infoYaml().documentation.outputs?.[pinIndex] ?? ''}</TableCell>
-                  <TableCell>{infoYaml().documentation.bidirectional?.[pinIndex] ?? ''}</TableCell>
+                  <TableCell>{projectInfo()?.pinout[`ui_in[${pinIndex}]`] ?? ''}</TableCell>
+                  <TableCell>{projectInfo()?.pinout[`uo_out[${pinIndex}]`] ?? ''}</TableCell>
+                  <TableCell>{projectInfo()?.pinout[`uio[${pinIndex}]`]}</TableCell>
                 </TableRow>
               )}
             </For>
