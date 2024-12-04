15621e17a97904c4dd4a6358a104c0814537af28
Uri Shaked <uri@urishaked.com>
2024-12-04 21:42:14 +0200

    style(ttcontrol): reformat with black

--- src/ttcontrol/ttcontrol.py
+++ src/ttcontrol/ttcontrol.py
@@ -3,9 +3,10 @@
 
 import os
 import sys
-import machine 
+import machine
 
 Timers = dict()
+
 
 def report(dict_or_key: dict, val: str = None):
     if val is not None and not isinstance(dict_or_key, dict):
@@ -13,7 +14,7 @@ def report(dict_or_key: dict, val: str = None):
 
     strs = list(map(lambda x: f"{x[0]}={x[1]}", dict_or_key.items()))
     print("\n".join(strs))
-    
+
 
 print()
 report("sys.version", sys.version.split(";")[1].strip())
@@ -31,6 +32,7 @@ FirstLoadDone = False
 
 tt = DemoBoard.get()
 
+
 def enable_ui_in(enabled):
     tt = DemoBoard.get()
     ac_freq = 0
@@ -44,7 +46,6 @@ def enable_ui_in(enabled):
         tt.mode = RPMode.ASIC_MANUAL_INPUTS
         start_monitoring(tt.ui_in, 10)
         start_monitoring(tt.uo_out, 10)
-        
 
     if ac_freq:
         set_clock_hz(ac_freq)
@@ -55,25 +56,27 @@ def enable_ui_in(enabled):
 def write_ui_in(data):
     DemoBoard.get().ui_in.value = data
 
+
 def dump_state():
     global Timers
     tt = DemoBoard.get()
     design = 0
     if tt.shuttle.enabled is not None:
         design = tt.shuttle.enabled.project_index
-        
+
     hz = tt.auto_clocking_freq
     vals = {
-        "tt.design": design, 
-        "tt.clk_freq": hz, 
+        "tt.design": design,
+        "tt.clk_freq": hz,
         "tt.mode": tt.mode_str,
-        "monitor": ','.join(Timers.keys())
+        "monitor": ",".join(Timers.keys()),
     }
     for io in [tt.ui_in, tt.uo_out, tt.uio_in]:
-        vals[f'tt.{io.port.name}'] = int(io.value)
-            
+        vals[f"tt.{io.port.name}"] = int(io.value)
+
     report(vals)
-    
+
+
 def select_design(design):
     tt = DemoBoard.get()
     tt.apply_configs = False
@@ -111,7 +114,6 @@ def manual_clock(cycles=1):
     report("tt.clk_once", cycles)
 
 
-# ROM format documented here: https://github.com/TinyTapeout/tt-chip-rom
 def read_rom():
     tt = DemoBoard.get()
     shuttle = tt.chip_ROM.shuttle
@@ -135,43 +137,39 @@ def run_factory_test():
         print("factory_test=OK")
 
 
-
 def start_monitoring(io, frequency):
     global Timers
-    name = io.port.name 
+    name = io.port.name
     if name in Timers:
-        if Timers[name]['freq'] == frequency:
+        if Timers[name]["freq"] == frequency:
             # already good
             return
-        Timers[name]['timer'].deinit()
-    
-    
-    Timers[name] = {
-        'timer':machine.Timer(),
-        'value': 0,
-        'freq': frequency
-    }
-    
+        Timers[name]["timer"].deinit()
+
+    Timers[name] = {"timer": machine.Timer(), "value": 0, "freq": frequency}
+
     def cb(t):
+        global Timers
         v = int(io.value)
-        if v != Timers[name]['value']:
-            Timers[name]['value'] = v
-            report(f'tt.{name}', v)
-    
-    Timers[name]['timer'].init(mode=machine.Timer.PERIODIC, freq=frequency, callback=cb)
+        if v != Timers[name]["value"]:
+            Timers[name]["value"] = v
+            report(f"tt.{name}", v)
+
+    Timers[name]["timer"].init(mode=machine.Timer.PERIODIC, freq=frequency, callback=cb)
+
 
 def stop_monitoring(io):
     global Timers
-    name = io.port.name 
+    name = io.port.name
     if name not in Timers:
-        return 
-        
-    Timers[name]['timer'].deinit()
+        return
+
+    Timers[name]["timer"].deinit()
     del Timers[name]
-    
+
+
 def stop_all_monitoring():
     global Timers
-    tt = DemoBoard.get() 
+    tt = DemoBoard.get()
     for name in Timers.keys():
         stop_monitoring(getattr(tt, name))
-        
